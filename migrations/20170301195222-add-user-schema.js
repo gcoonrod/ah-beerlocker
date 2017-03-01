e429884daa38cd7da02ae4f891471755050e8f70
Greg Coonrod <coogr02@ca.com>
2017-03-01 14:06:48 -0600

    Adding user model and migration

---
+++ migrations/20170301195222-add-user-schema.js
@@ -0,0 +1,40 @@
+'use strict'
+
+const Promise = require('bluebird')
+
+module.exports = {
+  up: function (queryInterface, DataTypes) {
+    return Promise.all([
+      queryInterface.createTable('users', {
+        id: {
+          type: DataTypes.UUID,
+          primaryKey: true,
+          defaultValue: DataTypes.UUIDV4
+        },
+        givenName: DataTypes.STRING,
+        familyName: DataTypes.STRING,
+        username: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          validate: {
+            isEmail: true
+          }
+        }
+      })
+    ])
+        .then(function () {
+          return Promise.all([
+            queryInterface.addIndex('users', ['username'], {
+              indexName: 'users_username_index',
+              indicesType: 'UNIQUE'
+            })
+          ])
+        })
+  },
+
+  down: function (queryInterface, DataTypes) {
+    return Promise.all([
+      queryInterface.dropTable('users')
+    ])
+  }
+}
